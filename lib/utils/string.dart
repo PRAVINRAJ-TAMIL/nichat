1f4132b11cb3f5ca0455bb14a9f8624b41981c42
PRAVINRAJ-TAMIL <114245769+PRAVINRAJ-TAMIL@users.noreply.github.com>
2024-02-06 15:09:38 +0530

    String

---
+++ lib/utils/string.dart
@@ -0,0 +1,6 @@
+class StringNichat{
+  static const String SIGNIN = "Sign In";
+  static const String SIGNUP = "Sign UP";
+  static const String SIGNIN = "SignIn";
+  static const String SIGNIN = "SignIn";
+}
