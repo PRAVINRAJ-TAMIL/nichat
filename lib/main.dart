2dad9db3ef7b77dab8834905f64013442ecbdf9d
PRAVINRAJ-TAMIL <114245769+PRAVINRAJ-TAMIL@users.noreply.github.com>
2024-02-06 15:01:31 +0530

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:neechat/BottomNavigation/auth_ser.dart';
@@ -14,9 +15,13 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  runApp(MyApp());
   runApp(const MyApp());
 }
-
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // Handle background notifications here
+}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
